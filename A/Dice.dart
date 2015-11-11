189e03b6912c98869f24fc517d6d756685eed66b
Andre <ndre.ajja@yahoo.com>
2015-11-11 11:00:44 +0700

    Create Dice.dart

---
+++ A/Dice.dart
@@ -0,0 +1,19 @@
+class Dice{
+	Random random;
+  
+  Dice(){
+  	random = new Random();
+  }
+  
+  num randomizer(){
+  	return random.nextInt(12)+1;
+  }
+}
+
+abstract class  Obstacle{
+  var image;
+  
+	void setObstacle(Tile tile);
+  void movePlayer(Player player);
+  
+}
