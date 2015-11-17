09420feb7a811bc47081c6fd5c40e6b237c1e4d6
claraccs <clara020896@gmail.com>
2015-11-17 11:01:26 +0700

    Update Comet.dart

--- A/Comet.dart
+++ A/Comet.dart
@@ -1,12 +1,18 @@
 import 'dart:math';
 import 'dart:core';
+import 'dart:html';
 
+	/// Kelas turunan [Obstacle] yang merepresentasikan komet.
 class Comet extends Obstacle{
-	//@Override
+	
+		//@Override
+		/// Method untuk mengeset [Tile] menjadi halangan.
     void setObstacle(Tile tile) {
         tile.setAsComet();
     }
 
+		//@Override
+		/// Method untuk memindahkan [Player] yang terkena halangan.
     void movePlayer(Player player) {
         Random rnd = new Random();
         int steps = rnd.nextInt(30)+1;
