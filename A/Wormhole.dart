baabdce640fbb2883a1ba01b1ab8006d8b2f018b
claraccs <clara020896@gmail.com>
2015-11-17 11:00:54 +0700

    Update Wormhole.dart

--- A/Wormhole.dart
+++ A/Wormhole.dart
@@ -1,14 +1,18 @@
-
 import 'dart:math';
 import 'dart:core';
+import 'dart:html';
 
+	/// Kelas turunan [Obstacle] yang merepresentasikan Wormhole.
 class Wormhole extends Obstacle{
-	//@Override
+	
+		//@Override
+		/// Method untuk mengeset [Tile] menjadi halangan.
     void setObstacle(Tile tile){
-  	tile.setAsWormhole();    
-  }
+		tile.setAsWormhole();    
+	}
 
-    //@Override
+		//@Override
+		/// Method untuk memindahkan [Player] yang terkena halangan.
     void movePlayer(Player player) {
         Random rnd = new Random();
         int steps = rnd.nextInt(30)+1;
