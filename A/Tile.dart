20d19f39c2a7d60c37246fe51dbb02322c4f64b5
claraccs <clara020896@gmail.com>
2015-11-17 10:53:20 +0700

    Update Tile.dart
    udah sama Dartdoc

--- A/Tile.dart
+++ A/Tile.dart
@@ -1,55 +1,61 @@
 import 'dart:math';
 import 'dart:core';
+import 'dart:html';
 
+	///Kelas ini merepresentasikan petak yang ada pada [Board].
 class Tile{
-		num _x;
-    num _y;
-    bool _isComet;
-    bool _isWormhole;
 
-    Tile(num x, num y) {
-        this._x = x;
-        this._y = y;
+		/// True jika petak adalah komet.
+    bool _isComet;
+	
+		/// True jika petak adalah wormhole.
+    bool _isWormhole;
+  	
+		/// True jika ada pemain di dalam petak.
+	bool _hasPlayer;
+
+		/// Constructor kelas [Tile].
+    Tile() {
         this._isComet = false;
         this._isWormhole = false;
     }
-
-    num getX() {
-        return this._x;
-    }
-
-    num getY() {
-        return this._y;
-    }
-
-    void setX(int x) {
-        this._x = x;
-    }
-
-    void setY(int y) {
-        this._y = y;
-    }
-
+	
+		/// Mengembalikan nilai atribut [_isComet].
     bool isComet() {
         return this._isComet;
     }
 
+		/// Mengembalikan nilai atribut [_isWormhole].
     bool isWormhole() {
         return this._isWormhole;
     }
 
+		/// Mengeset petak menjadi wormhole.
     void setAsWormhole() {
         this._isWormhole = true;
     }
 
+		/// Mengeset petak menjadi komet.
     void setAsComet() {
         this._isComet = true;
     }
-
+	
+		/// Mengeset nilai atribut [_hasPlayer] menjadi true.
+  	void setHasPlayer() {
+        this._hasPlayer = true;
+    }
+    
+		/// Method yang merepresentasikan pemain meninggalkan permainan. 
+		/// Mengeset nilai atribut [_hasPlayer] menjadi false.
+	void setPlayerLeft() {
+        this._hasPlayer = false;
+    }
+  
+		/// Method yang mengembalikan tampilan petak dalam text mode.
     String toString(){
         if(_isComet) return " * ";
         else if(_isWormhole) return " O ";
+        else if(_hasPlayer) return " + ";
         else return " . ";
     }
-
 }
