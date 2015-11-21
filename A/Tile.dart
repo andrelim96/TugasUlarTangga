4a0b4f560019e12b2a87e915a3ee4808dc359bf0
marchellametta <morningracoon@gmail.com>
2015-11-21 17:00:00 +0700

    Update Tile.dart

--- A/Tile.dart
+++ A/Tile.dart
@@ -1,61 +1,52 @@
-import 'dart:math';
-import 'dart:core';
-import 'dart:html';
-
-	///Kelas ini merepresentasikan petak yang ada pada [Board].
 class Tile{
+  bool _hasPlayer;
+  bool _isCometEnd;
+  bool _isWormholeEnd;
+  bool _isSetable;
 
-		/// True jika petak adalah komet.
-    bool _isComet;
-	
-		/// True jika petak adalah wormhole.
-    bool _isWormhole;
-  	
-		/// True jika ada pemain di dalam petak.
-	bool _hasPlayer;
+  Tile() {
+    this._hasPlayer = false;
+    this._isCometEnd = false;
+    this._isWormholeEnd = false;
+    this._isSetable = true;
+  }
+  void setHasPlayer() {
+    this._hasPlayer = true;
+  }
 
-		/// Constructor kelas [Tile].
-    Tile() {
-        this._isComet = false;
-        this._isWormhole = false;
-    }
-	
-		/// Mengembalikan nilai atribut [_isComet].
-    bool isComet() {
-        return this._isComet;
-    }
+  void setPlayerLeft() {
+    this._hasPlayer = false;
+  }
 
-		/// Mengembalikan nilai atribut [_isWormhole].
-    bool isWormhole() {
-        return this._isWormhole;
-    }
-
-		/// Mengeset petak menjadi wormhole.
-    void setAsWormhole() {
-        this._isWormhole = true;
-    }
-
-		/// Mengeset petak menjadi komet.
-    void setAsComet() {
-        this._isComet = true;
-    }
-	
-		/// Mengeset nilai atribut [_hasPlayer] menjadi true.
-  	void setHasPlayer() {
-        this._hasPlayer = true;
-    }
-    
-		/// Method yang merepresentasikan pemain meninggalkan permainan. 
-		/// Mengeset nilai atribut [_hasPlayer] menjadi false.
-	void setPlayerLeft() {
-        this._hasPlayer = false;
-    }
+  String toString(){
+    if(_hasPlayer) return " + ";
+    else return " . ";
+  }
   
-		/// Method yang mengembalikan tampilan petak dalam text mode.
-    String toString(){
-        if(_isComet) return " * ";
-        else if(_isWormhole) return " O ";
-        else if(_hasPlayer) return " + ";
-        else return " . ";
-    }
+  bool isCometEnd()
+  {
+    return this._isCometEnd;
+  }
+  
+  void setCometEnd()
+  {
+    this._isCometEnd = true;
+    this._isSetable = false;
+  }
+  
+  bool isWormholeEnd()
+  {
+    return this._isWormholeEnd;
+  }
+  bool isSetable()
+  {
+    return this._isSetable;
+  }
+  
+  void setWormholeEnd()
+  {
+    this._isWormholeEnd = true;
+    this._isSetable = false;
+  }
+
 }
