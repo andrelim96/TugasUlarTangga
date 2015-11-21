aa566b334e8e434a95a8c0f11ee5643202a9bc64
marchellametta <morningracoon@gmail.com>
2015-11-21 17:36:00 +0700

    Update Obstacle.dart

--- A/Obstacle.dart
+++ A/Obstacle.dart
@@ -1,22 +1,28 @@
+///Kelas abstrak yang merepresentasikan rintangan dalam papan permainan ular tangga.
 abstract class  Obstacle extends Tile{
+  ///atribut yang menyimpan index letak awal rintangan dalam papan permainan.
   int _in;
+   ///atribut yang menyimpan index letak akhir rintangan dalam papan permainan.
   int _out;
   Random random = new Random();
   
+  ///Method yang mengembalikan index letak akhir rintangan dalam papan permainan.
   int getOutIndex()
   {
     return this._out;
   }
   
+  ///Method yang mengembalikan index letak akhir rintangan dalam papan permainan.
    int getInIndex()
   {
     return this._in;
   }
+  ///Method yang mengubah nilai index letak akhir rintangan dalam papan permainan dengan parameter nilai pengganti index letak akhir.
   void setOutIndex(int x)
   {
     this._out=x;
   }
-  
+  ///Method yang mengubah nilai index letak awal rintangan dalam papan permainan dengan parameter nilai pengganti index letak awal.
    void setInIndex(int x)
   {
     this._in = x;
